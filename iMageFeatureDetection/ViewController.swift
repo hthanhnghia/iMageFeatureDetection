e15cb5fda5110f89c3cf7cabac3b78207344c743
Ho Nghia <hothanhnghia@Quachs-MacBook-Pro.local>
2017-03-25 03:13:17 +0800

    Initial Commit

---
+++ iMageFeatureDetection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iMageFeatureDetection
+//
+//  Created by Ho Thanh Nghia on 25/3/17.
+//  Copyright © 2017 NghiaHo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
